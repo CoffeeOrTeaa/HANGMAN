84643d40efbffed05875b05dcf712253907bf307
CoffeeOrTeaa <aleksandarkojic2000@gmail.com>
2022-12-21 20:02:34 +0100

    Add project files.

---
+++ HANGMAN/Source.cpp
@@ -0,0 +1,37 @@
+/*************************************************
+* Dateiname: Cos_Programmieren
+* Autor: cbu
+* Datum:31.10.2022
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+* Zweck:
+* Eingaben:
+* Ausgaben:
+************************************************/
+
+
+
+#include <iostream>
+
+using namespace std;
+
+
+
+
+
+int main()
+{
+    cout << "Welcome to" << endl;
+    cout << "HANGMAN" << endl;
+    cout << "The Rules are:" << endl;
+    cout << " 1. Der Spielleiter sucht sich ein Wort aus und Tippt es ein, waehrend die anderen wegschauen - nicht spicken!";
+    cout << " . Der Spielleiter sucht sich ein Wort aus und Tippt es ein, waehrend die anderen wegschauen - nicht spicken!";
+
+
+
+
+
+
+    return 0;
+}
+
+
