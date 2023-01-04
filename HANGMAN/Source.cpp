8db8b2ab51e3b1404adc6ee719bf6d132335c2c2
CoffeeOrTeaa <aleksandarkojic2000@gmail.com>
2023-01-04 10:00:05 +0100

    #

--- HANGMAN/Source.cpp
+++ HANGMAN/Source.cpp
@@ -22,10 +22,13 @@ int main()
 {
     cout << "Welcome to" << endl;
     cout << "HANGMAN" << endl;
-    cout << "The Rules are:" << endl;
-    cout << " 1. Der Spielleiter sucht sich ein Wort aus und Tippt es ein, waehrend die anderen wegschauen - nicht spicken!";
-    cout << " . Der Spielleiter sucht sich ein Wort aus und Tippt es ein, waehrend die anderen wegschauen - nicht spicken!";
-
+    cout << "Die Regeln:" << endl;
+    cout << " 1. Der Spielleiter sucht sich ein Wort aus und Tippt es ein, waehrend die anderen wegschauen - nicht spicken!"<< endl;
+    cout << " 2. Das Raten faengt an, jeder aussser der Spielleiter ist ein/e Rater/-in!" << endl << "    Jeder aussser der Spielleiter ratet einen Buchstaben. Wer ein ganzes Wort erraten will, faengt das Wort mit einem '!' an!" << endl;
+    cout << " 3. Erratene Buchstaben werden auf dem Bildschirm angezeigt und koennen nicht mehr vorgeschlagen werden!" << endl << "    Falls das Wort von den Ratern/Raterinnen nicht rechtzeitig erraten wird, dann hat der Spielleiter gewonnen" << endl << "    und die Rater / Raterinnen verloren!"
+        << "    Wie viele Versuche die Rater/-innen noch haben wird als Zahl und als Strichmaennchen aufgezeigt!" << endl<< "    - Ist das Strichmaennchen fertig, dann sind die Veruche aus und der Spielleiter hat gewonnen!";
+    cin.ignore();
+    system("cls");
 
 
 
